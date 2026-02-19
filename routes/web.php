fb8e21cc2df6b67faa1abe905f14fa35924dc6c9
Siddharth <siddharth@localhost.localdomain>
2026-02-19 21:18:13 +0530

    msg

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StationeryController;
 
 
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 
 Route::get('/home', function () {
     return view('welcome');
